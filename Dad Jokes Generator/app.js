97159208ac81c23c1b2cae8ad4367402fefd7afb
SoupD0gg <121968951+SoupD0gg@users.noreply.github.com>
2023-06-05 16:16:23 +0100

    Add files via upload

---
+++ Dad Jokes Generator/app.js
@@ -0,0 +1,33 @@
+const btnEl = document.getElementById("btn");
+const jokeEl = document.getElementById("joke");
+const apiKey = "Y1tqmqwIhwKKsvBUfBiDiA==ZQS8Put0RbvBLoMo";
+const apiURL = "https://api.api-ninjas.com/v1/dadjokes?limit=1";
+const options = {
+  method: "GET",
+  headers: {
+    "X-Api-Key": apiKey,
+  },
+};
+
+async function getJoke() {
+
+  try {
+    jokeEl.innerText = "Updating...";
+    btnEl.disabled = true;
+    btnEl.innerText = "Loading...";
+    const response = await fetch(apiURL, options);
+    const data = await response.json();
+    jokeEl.innerText = data[0].joke
+
+    btnEl.disabled = false;
+    btnEl.innerText = "Tell me a joke!";
+  } catch (error) {
+    jokeEl.innerText = "An Error has occurred! Try again later.";
+    btnEl.disabled = false;
+    btnEl.innerText = "Tell me a joke!";
+    console.log(error);
+  }
+
+};
+
+btnEl.addEventListener("click", getJoke);
